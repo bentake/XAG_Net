4686f3ce46b7cf7f33aeae60cb7416adefa73ef8
Byunghyun Ko <101140864+bentake@users.noreply.github.com>
2025-08-25 17:24:13 -0700

    Update models.py - filter fix

--- xagnet/models.py
+++ xagnet/models.py
@@ -21,28 +21,34 @@ def build_xagnet_unet(input_shape=(256,256,3), num_classes=1):
     inp = Input(input_shape)
     refined = cross_slice_attention(inp)
 
-    c1 = Conv2D(32, 3, padding="same", activation="relu")(refined); c1 = BatchNormalization()(c1); p1 = MaxPooling2D(2)(c1)
-    c2 = Conv2D(64, 3, padding="same", activation="relu")(p1); c2 = BatchNormalization()(c2); p2 = MaxPooling2D(2)(c2)
-    c3 = Conv2D(128, 3, padding="same", activation="relu")(p2); c3 = BatchNormalization()(c3); p3 = MaxPooling2D(2)(c3)
-    c4 = Conv2D(256, 3, padding="same", activation="relu")(p3); c4 = BatchNormalization()(c4); p4 = MaxPooling2D(2)(c4)
+    inp = Input(input_shape)
+    refined = cross_slice_attention(inp)
 
-    bn = Conv2D(512, 3, padding="same", activation="relu")(p4); bn = BatchNormalization()(bn)
+    # Encoder: 64, 128, 256, 512
+    c1 = Conv2D(base_filters, 3, padding="same", activation="relu")(refined); c1 = BatchNormalization()(c1); p1 = MaxPooling2D(2)(c1)
+    c2 = Conv2D(base_filters*2, 3, padding="same", activation="relu")(p1);    c2 = BatchNormalization()(c2); p2 = MaxPooling2D(2)(c2)
+    c3 = Conv2D(base_filters*4, 3, padding="same", activation="relu")(p2);    c3 = BatchNormalization()(c3); p3 = MaxPooling2D(2)(c3)
+    c4 = Conv2D(base_filters*8, 3, padding="same", activation="relu")(p3);    c4 = BatchNormalization()(c4); p4 = MaxPooling2D(2)(c4)
 
-    u5 = Conv2DTranspose(256, 3, strides=2, padding="same", activation="relu")(bn)
-    a5 = attention_block(c4, u5, 256); cs5 = cross_slice_attention(c4)
-    u5 = concatenate([u5, a5, cs5]); u5 = Conv2D(256, 3, padding="same", activation="relu")(u5); u5 = BatchNormalization()(u5)
+    # Bottleneck: 1024
+    bn = Conv2D(base_filters*16, 3, padding="same", activation="relu")(p4);   bn = BatchNormalization()(bn)
 
-    u6 = Conv2DTranspose(128, 3, strides=2, padding="same", activation="relu")(u5)
-    a6 = attention_block(c3, u6, 128); cs6 = cross_slice_attention(c3)
-    u6 = concatenate([u6, a6, cs6]); u6 = Conv2D(128, 3, padding="same", activation="relu")(u6); u6 = BatchNormalization()(u6)
+    # Decoder: 512, 256, 128, 64 (with AG + CSA at each skip)
+    u5 = Conv2DTranspose(base_filters*8, 3, strides=2, padding="same", activation="relu")(bn)
+    a5 = attention_block(c4, u5, base_filters*8); cs5 = cross_slice_attention(c4)
+    u5 = concatenate([u5, a5, cs5]); u5 = Conv2D(base_filters*8, 3, padding="same", activation="relu")(u5); u5 = BatchNormalization()(u5)
 
-    u7 = Conv2DTranspose(64, 3, strides=2, padding="same", activation="relu")(u6)
-    a7 = attention_block(c2, u7, 64); cs7 = cross_slice_attention(c2)
-    u7 = concatenate([u7, a7, cs7]); u7 = Conv2D(64, 3, padding="same", activation="relu")(u7); u7 = BatchNormalization()(u7)
+    u6 = Conv2DTranspose(base_filters*4, 3, strides=2, padding="same", activation="relu")(u5)
+    a6 = attention_block(c3, u6, base_filters*4); cs6 = cross_slice_attention(c3)
+    u6 = concatenate([u6, a6, cs6]); u6 = Conv2D(base_filters*4, 3, padding="same", activation="relu")(u6); u6 = BatchNormalization()(u6)
 
-    u8 = Conv2DTranspose(32, 3, strides=2, padding="same", activation="relu")(u7)
-    a8 = attention_block(c1, u8, 32); cs8 = cross_slice_attention(c1)
-    u8 = concatenate([u8, a8, cs8]); u8 = Conv2D(32, 3, padding="same", activation="relu")(u8); u8 = BatchNormalization()(u8)
+    u7 = Conv2DTranspose(base_filters*2, 3, strides=2, padding="same", activation="relu")(u6)
+    a7 = attention_block(c2, u7, base_filters*2); cs7 = cross_slice_attention(c2)
+    u7 = concatenate([u7, a7, cs7]); u7 = Conv2D(base_filters*2, 3, padding="same", activation="relu")(u7); u7 = BatchNormalization()(u7)
+
+    u8 = Conv2DTranspose(base_filters, 3, strides=2, padding="same", activation="relu")(u7)
+    a8 = attention_block(c1, u8, base_filters);   cs8 = cross_slice_attention(c1)
+    u8 = concatenate([u8, a8, cs8]); u8 = Conv2D(base_filters, 3, padding="same", activation="relu")(u8); u8 = BatchNormalization()(u8)
 
     out = Conv2D(num_classes, 1, activation="sigmoid")(u8)
     return Model(inp, out)
